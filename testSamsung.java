3bbfcf396c97901206ebf2761f5ce69e363fc068
Rushikesh Shete <144946261+rushishete1818@users.noreply.github.com>
2025-01-29 00:32:23 +0530

    Add files via upload

---
+++ testSamsung.java
@@ -0,0 +1,25 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class testSamsung {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration();
+		cfg.addAnnotatedClass(Samsung.class).configure();
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session ss = sf.openSession();
+		Transaction tx = ss.beginTransaction();
+		
+		//Samsung s = new Samsung(101,"shimjang", 150000);
+		//ss.save(s); //this insert the single data in a database
+		Samsung s = new Samsung(101,"chimav",200000); 
+		ss.update(s); //this is used for update the data of database
+		tx.commit();
+		ss.close();
+
+	}
+
+}
